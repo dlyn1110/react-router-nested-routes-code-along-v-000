284e77180c0d58ac9089eeb650707fb79ce040ba
Dani Lyn <dlyn1110@gmail.com>
2019-05-23 19:48:49 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 import MovieShow from './MovieShow';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MoviesPage = ({ match, movies }) => (
   <div>
