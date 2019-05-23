e2065289d3afa2c7ef8c0be6357111d9ff4b9e94
Dani Lyn <dlyn1110@gmail.com>
2019-05-23 19:52:51 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,17 +1,19 @@
 // .src/containers/MoviesPage.js
+// .src/containers/MoviesPage.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import MoviesList from '../components/MoviesList';
+import MovieShow from './MovieShow';
 
-const MoviesList = ({ movies }) => {
-  const renderMovies = Object.keys(movies).map(movieID =>
-    <Link key={movieID} to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
-  );
+const MoviesPage = ({ match, movies }) => (
+  <div>
+    <MoviesList movies={movies} />
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a Movie from the list.</h3>
+    )}/>
+    <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow movies={movies} {...routerProps} /> }/>
+  </div>
 
-  return (
-    <div>
-      {renderMovies}
-    </div>
-  );
-};
+)
 
-export default MoviesList
+export default MoviesPage
